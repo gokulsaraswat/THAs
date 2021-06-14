e82e0fbebbce9d7f438469fada0f2584e6797253
kennei <1by19is062@bmsit.in>
2021-06-15 00:00:25 +0530

    15th Day 6th and Day 7th THA Done

---
+++ THA_day7/app.js
@@ -0,0 +1,28 @@
+/* // 1st question
+
+
+var student ={name : "David Rayy", sclass : "VI", rollno : 12} 
+var result = Object.values(student)
+
+
+console.log("Before deleting: ",result.toString());
+
+// 3rd question
+console.log("Object Length: ",Object.keys(student).length);
+
+// 2nd question
+delete student.rollno;
+// Object.pop;
+console.log("After deleting: ",Object.values(student).toString());
+ */
+
+//4rth question
+// let library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }];
+// console.log((Object.keys(library[0])).toString());
+// for(let i = 0; i < library.length;i++){
+
+// 	console.log((Object.values(library[i])).toString());
+
+// }
+
+
