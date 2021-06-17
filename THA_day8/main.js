ec73a1f386c901eb1e3af58be0fa4bce51494292
kennei <1by19is062@bmsit.in>
2021-06-18 01:14:17 +0530

    d

--- THA_day8/main.js
+++ THA_day8/main.js
@@ -3,6 +3,6 @@ let squares = document.getElementsByClassName('baksa');
 
 Array.prototype.forEach.call(squares, (el)=>{
 el.addEventListener('keypress', (event)=>{
-
+	
     el.classList.toggle('active');})
 });
