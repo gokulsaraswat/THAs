556747a36cf52ba602e02e0caba813b3ff8af69a
kennei <1by19is062@bmsit.in>
2021-06-18 00:54:49 +0530

    grid playing css

---
+++ THA_day8/main.js
@@ -0,0 +1,8 @@
+
+let squares = document.getElementsByClassName('baksa');
+
+Array.prototype.forEach.call(squares, (el)=>{
+el.addEventListener('keypress', (event)=>{
+
+    el.classList.toggle('active');})
+});
