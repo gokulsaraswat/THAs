8126c33f846c97969a833b22ecbd177ea0066cd4
kennei <1by19is062@bmsit.in>
2021-06-19 21:53:27 +0530

    19 06 2021

---
+++ Temp/temp.py
@@ -0,0 +1,18 @@
+# Please Make a software for a mobile service center in there is two option one for
+# customer and another for customer service officer. Whenever a customer enter his
+# details like name, device name and issue with mobile than his token no. is
+# generated automatically without invoking any method explicitly and if the system
+# already generated III token then that display a message about “sorry already III
+# persons in queue” and also a customer service officer choose a option query is
+# processed after passing its credentials then next customer will capable for token
+# generation.
+
+print('Mobile service center')
+
+
+option=input('Press 1. for customer and 2. for customer service officer' )
+
+switch(option){
+	case 1:
+	case 2:
+}
