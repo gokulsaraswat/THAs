f6153b97738b6ae38759851e0d866ac808793185
kennei <1by19is062@bmsit.in>
2021-06-19 23:38:10 +0530

    THA 9

--- THA_day8/main.js
+++
@@ -1,8 +0,0 @@
-
-let squares = document.getElementsByClassName('baksa');
-
-Array.prototype.forEach.call(squares, (el)=>{
-el.addEventListener('keypress', (event)=>{
-	
-    el.classList.toggle('active');})
-});
---
+++ THA_day9/main.js
@@ -0,0 +1,56 @@
+
+var total = 36;
+var taken = 0;
+
+const box = document.getElementsByClassName('allseats');
+
+Array.prototype.forEach.call(box, (el)=>{
+	el.addEventListener('click', (event)=>{
+
+		
+		if(el.classList.contains('active'))
+		{
+			taken -=1;
+			el.classList.remove('active');
+			document.getElementById("RemainingSeats").innerHTML = total-taken; 
+			document.getElementById("BookedSeats").innerHTML = taken; 
+		}
+
+		else{
+			taken +=1;
+			el.classList.add('active');
+			document.getElementById("RemainingSeats").innerHTML = total-taken; 
+			document.getElementById("BookedSeats").innerHTML = taken;
+		}
+
+	})
+
+});
+
+
+
+// let squares = document.getElementsByClassName('baksa');
+
+// Array.prototype.forEach.call(squares, (el)=>{
+// el.addEventListener('mouseover', (event)=>{
+	
+//     el.classList.toggle('active');})
+// });	
+
+
+// var total = 36;
+// var selectedseats=0;
+// var remaining= total - selectedseats;
+
+
+// const box = document.getElementsByClassName('allseats');
+
+// Array.prototype.forEach.call(box, (el)=>{
+// 	el.addEventListener('click', (event)=>{
+// 		el.classList.toggle('active'); })
+
+// });
+
+
+// document.getElementById("RemainingSeats").innerHTML = remaining; 
+// document.getElementById("BookedSeats").innerHTML = selectedseats; 
