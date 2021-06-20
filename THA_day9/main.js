af2a03af8838926ea792b6dd1acb03ff30365d43
kennei <1by19is062@bmsit.in>
2021-06-20 12:39:16 +0530

    x sauce bhai gujrati man help

--- THA_day9/main.js
+++ THA_day9/main.js
@@ -2,6 +2,16 @@
 var total = 36;
 var taken = 0;
 
+let totalseats = 50
+var seatsdiv = document.getElementById('seats')
+for (let index = 0; index < totalseats; index++) {
+    const seatelement = document.createElement("div");
+    seatelement.setAttribute('id',index) //giving an id just to recall in js , diffrent id's for each !
+    seatelement.setAttribute('class',"seats")
+    document.body.insertBefore(seatelement, seatsdiv);
+}
+
+
 const box = document.getElementsByClassName('allseats');
 
 Array.prototype.forEach.call(box, (el)=>{
@@ -46,13 +56,10 @@ Array.prototype.forEach.call(box, (el)=>{
 
 
 // const box = document.getElementsByClassName('allseats');
-
 // Array.prototype.forEach.call(box, (el)=>{
 // 	el.addEventListener('click', (event)=>{
 // 		el.classList.toggle('active'); })
 
 // });
-
-
 // document.getElementById("RemainingSeats").innerHTML = remaining; 
 // document.getElementById("BookedSeats").innerHTML = selectedseats; 
